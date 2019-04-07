c4ee0ed8f5d054a597147e2571617b473f6f3630
NickNowe-nscc <w0134248@nscc.ca>
2019-04-07 19:19:13 -0300

    test

--- src/utils/PostgresUtil.js
+++ src/utils/PostgresUtil.js
@@ -1,10 +1,8 @@
 const { Pool } = require('pg')
 
 const pool = new Pool({
-  // connectionString: process.env.DATABASE_URL,
+  connectionString: process.env.DATABASE_URL,
   // ssl: true,
-          user: "osys1000",
-          host: "localhost",
           password: "student",
           database: "postgres"
 
